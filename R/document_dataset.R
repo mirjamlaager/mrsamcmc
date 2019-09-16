e10aad9927728070ae5d6250fbb71d40e09387e1
Mirjam Laager <mirjam.laager@ndm.ox.ac.uk>
2019-09-16 14:42:21 +0100

    changed dataset to a dataset including types. updated documentation of the dataset and introduction vignette accordingly.

--- R/document_dataset.R
+++ R/document_dataset.R
@@ -1,22 +1,23 @@
 #' Simulated patient data.
 #'
 #' A dataset containing admission and discharge dates, test
-#' results and antibiotic use of 450 patients over 90 days.
+#' results and antibiotic use of 270 patients over 90 days.
 #'
 #'
 #' @format a list containing 4 elements:
 #' \describe{
-#'   \item{patients}{a dataframe with three columns and 450 rows. Each row
-#'   corresponds to a patient, the columns are admission, first_positive_test
-#'   and discharge in days. If there is no positive test for a patient,
-#'   first_positive_test equals 20000.}
-#'   \item{test_results_positive}{a matrix with 450 rows and 30 columns. Each
+#'   \item{patients}{a dataframe with four columns and 270 rows. Each row
+#'   corresponds to a patient, the columns are admission, type,
+#'   first_positive_test and discharge in days. If there is no type for a patient,
+#'   type equals -1. If there is no positive test for a patient, first_positive_test
+#'   equals 20000.}
+#'   \item{test_results_positive}{a matrix with 270 rows and 30 columns. Each
 #'   row corresponds to a patient. Entries correspond to the day of a positive
 #'   test.}
-#'    \item{test_results_negative}{a matrix with 450 rows and 30 columns. Each
+#'    \item{test_results_negative}{a matrix with 270 rows and 30 columns. Each
 #'   row corresponds to a patient. Entries correspond to the day of a negative
 #'   test.}
-#'    \item{antibiotics}{a matrix with 450 rows and 100 columns. Each
+#'    \item{antibiotics}{a matrix with 270 rows and 100 columns. Each
 #'   row corresponds to a patient. Entries correspond to the day when an
 #'   antibiotic was administered.}
 #' }
